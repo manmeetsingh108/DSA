1e866fdc46a3f2b848f6a13bd99f112eec60794f
manmeetsingh108 <2025a1r018@mietjammu.in>
2026-02-06 13:34:43 +0530

    Update and rename IMPLEMENTING LIST USING ARRAY.c to IMPLEMENTING_LIST_USING ARRAY.c

---
+++ IMPLEMENTING_LIST_USING ARRAY.c
@@ -0,0 +1,66 @@
+//IMPLEMENTING LIST USING ARRAY 
+
+#include<stdio.h>
+#define MAX 100
+int list[MAX];
+int n = 0;
+void insertInBeg(int value){
+    if(size > max){
+        printf("List is full!!!!!!");
+    }
+    else{
+        
+    }
+}
+int main(){
+    while(1){
+        printf("1. Insert in Beginning\n");
+        printf("2. Insert at End\n");
+        printf("3. Insert at Any Position\n");
+        printf("4. Delete from Beginning\n");
+        printf("5. Delete from End\n");
+        printf("6. Delete from Given Position\n");
+        printf("7. Display List\n");
+        printf("8. Search Element in List\n");
+        printf("9. Update Element of List\n");
+        printf("10.Sort Element of List\n");
+        printf("11.exit\n");
+
+        int choice;
+        printf("Enter Choice: ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            default:
+                printf("Invalid Choice !!!!!!!\n");
+        }
+
+    }
+    return 0;
+}
+
+
+
+
+
+
+    
