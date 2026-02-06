90761b40fc868cc42f64faeca596f4d94ccddb1b
manmeetsingh108 <2025a1r018@mietjammu.in>
2026-02-06 12:56:57 +0530

    Create IMPLEMENTING LIST USING ARRAY.c

---
+++ IMPLEMENTING LIST USING ARRAY.c
@@ -0,0 +1,65 @@
+//IMPLEMENTING LIST USING ARRAY 
+
+#include<stdio.h>
+#define max 100
+int size=0;
+void inserinbeg(int value){
+    if(size>max)
+    printf("lList is full!!!!!!");
+    else{
+
+
+    }
+}
+int main(){
+    while(1){
+        printf("1. insert in beginning\n");
+        printf("2. insert at end\n");
+        printf("3. insert at specified position\n");
+        printf("4. delete from beginning\n");
+        printf("5. delete from end\n");
+        printf("6. delete from Given Position\n");
+        printf("7. display list\n");
+        printf("8. search element in list\n");
+        printf("9. update list element\n");
+        printf("10.sort element of list\n");
+        printf("11.exit\n");
+
+        int choice;
+        printf("Enter Choice:");
+        scanf("%d,&choice");
+        switch(choice){
+         case 1:
+            break;
+         case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        default:
+        printf("Invalid Choice !!!!!!!\n");
+        }
+
+    }
+        return 0;
+    }
+
+
+
+
+
+
+    
